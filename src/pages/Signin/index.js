1b9a255633f2dbae805f1b2872ec9ffd7a5a03b2
Gustavo Neitzke <gutoneitzke12345@gmail.com>
2022-12-31 12:15:44 -0300

    feat: new style adjusts to signin component

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,10 +1,99 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+import * as Animatable from 'react-native-animatable';
 
 export default function Sigin() {
  return (
-    <View>
-        <Text>tela de login</Text>
+    <View style={styles.container}>
+      <Animatable.View
+        animation="fadeInLeft"
+        delay={500}
+        style={styles.containerHeader}
+      >
+        <Text style={styles.message}>Bem-vindo(a)</Text>
+      </Animatable.View>
+
+      <Animatable.View
+        animation="fadeInUp"
+        style={styles.containerForm}
+      >
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          placeholder="Digite um email..."
+          style={styles.input}
+        />
+
+        <Text style={styles.label}>Senha</Text>
+        <TextInput
+          placeholder="Digite sua senha..."
+          style={styles.input}
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttonRegister}>
+          <Text style={styles.buttonRegisterText}>Não possui conta? Cadastre-se</Text>
+        </TouchableOpacity>
+      </Animatable.View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#38a69d'
+  },
+  containerHeader: {
+    marginTop: '14%',
+    marginBottom: '8%',
+    paddingStart: '5%'
+  },
+  message: {
+    fontSize: 28,
+    color: '#fff',
+    fontWeight: '500'
+  },
+  containerForm: {
+    backgroundColor: '#fff',
+    flex: 1,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: '4%',
+    paddingEnd: '4%'
+  },
+  label: {
+    fontSize: 20,
+    marginTop: 28
+  },
+  input: {
+    borderBottomWidth: 1,
+    height: 40,
+    marginBottom: 12,
+    fontSize: 16
+  },
+  button: {
+    backgroundColor: '#38a69d',
+    width: '100%',
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginTop: 14,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '500'
+  },
+  buttonRegister: {
+    marginTop: 14,
+    alignSelf: 'center'
+  },
+  buttonRegisterText: {
+    color: '#a1a1a1'
+  }
+})
